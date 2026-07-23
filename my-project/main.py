ae886d857f6252be0e6e090df6138f95dbdeba6f
Juliet Mewett <juliet.mewett@Imp.net>
2026-07-23 12:22:51 +0100

    removed media fold, added two transforms technique

--- my-project/main.py
+++ my-project/main.py
@@ -34,6 +34,28 @@ class SquareAndCircle(Scene):
         square.next_to(circle, RIGHT, buff=0.5)
         self.play(Create(circle), Create(square))
 
+class TwoTransforms(Scene):
+    def transform(self):
+        a = Circle()
+        b = Square()
+        c = Triangle()
+        self.play(Transform(a, b))
+        self.play(Transform(a, c))
+        self.play(FadeOut(a))
+
+    def replacement_transform(self):
+        a = Circle()
+        b = Square()
+        c = Triangle()
+        self.play(ReplacementTransform(a, b))
+        self.play(ReplacementTransform(b, c))
+        self.play(FadeOut(c))
+
+    def construct(self):
+        self.transform()
+        self.wait(0.5)
+        self.replacement_transform()
+
 class ShowScreenResolution(Scene):
     def construct(self):
         pixel_height = config["pixel_height"]  #  1080 is default
